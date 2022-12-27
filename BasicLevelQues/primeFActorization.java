ae48e9015d583486fb58860ec99d84d0eed4197f
Amir <amirbhai432@gmail.com>
2022-12-27 21:07:10 +0530

    Butterfly Pattern

--- BasicLevelQues/primeFActorization.java
+++ BasicLevelQues/primeFActorization.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 
 public class primeFActorization {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int div = 2; div * div <= n; div++) {
-            while (n != 0) {
-                n = n / 10;
-                System.out.print(div + " ");
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            for (int div = 2; div * div <= n; div++) {
+                while (n != 0) {
+                    n = n / 10;
+                    System.out.print(div + " ");
+                }
             }
-        }
-        if (n != 0) {
-            System.out.print(n);
+            if (n != 0) {
+                System.out.print(n);
+            }
         }
 
     }

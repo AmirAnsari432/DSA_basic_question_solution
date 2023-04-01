e2f84dd0b8ffd371bf7c89da08595615dadeadc9
Amir <amirbhai432@gmail.com>
2023-04-02 00:16:08 +0530

    pattern ques

---
+++ Oprators/ques1.java
@@ -0,0 +1,72 @@
+package Oprators;
+
+import java.util.Scanner;
+
+public class ques1 {
+    public static void main(String[] args) {
+        // int x = 2, y = 5;
+        // int exp1 = (x * y / x);
+        // int exp2 = (x * (y / x));
+        // System.out.print(exp1 + " , ");
+        // System.out.print(exp2);
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter a number ");
+
+        // int x = sc.nextInt();
+        // if (x > 0) {
+        // System.out.println("x is greater then zero this is positive");
+        // } else {
+        // System.out.println("x is less then and equal to 0 this is negative number ");
+        // }
+        // double temp = 99;
+
+        // if (temp >= 100) {
+        // System.out.println("you are not fine ");
+        // } else {
+        // System.out.println("you are fine ");
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a number");
+        // int Days = sc.nextInt();
+
+        // switch (Days) {
+        // case 1:
+        // System.out.println("Today is Monday");
+        // break;
+        // case 2:
+        // System.out.println("Today is Tuesday");
+        // break;
+        // case 3:
+        // System.out.println("Today is Wednesday ");
+        // break;
+
+        // case 4:
+        // System.out.println("Today is Thursday");
+        // break;
+
+        // case 5:
+        // System.out.println("Today is Friday");
+        // break;
+
+        // case 6:
+        // System.out.println("Today is Saturday");
+        // break;
+
+        // case 7:
+        // System.out.println("Today is Sunday");
+        // break;
+
+        // }
+
+        // int counter = 0;
+        // while (counter < 10) {
+        //     System.out.println(counter + " Hello World");
+        //     counter++;
+        // }
+
+
+        
+    }
+}
